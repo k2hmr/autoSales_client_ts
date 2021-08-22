e86e69716aa24eb7651c67db65177d74ff86147d
kajimura <k2hmr94@gmail.com>
2021-08-22 18:57:33 +0900

    ‘API叩く実装’

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import JSONDATA from './test.json';
+import { Link } from 'react-router-dom';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  
+  const search = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const input = form.querySelector('#searchText') as HTMLInputElement;
+    setSearchTerm(input.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>自動営業ツール</h1>
+      <form className="searchForm" onSubmit={event => search(event)}>
+        <input
+          id="searchText"
+          type="text"
+          placeholder="営業したい企業名のキーワードを入力"
+        />
+        <button>検索</button>
+        {JSONDATA.filter((val) => {
+          if(searchTerm === ""){
+            return val
+          } else if(val.name.toLowerCase().includes(searchTerm.toLowerCase())){
+            return val;
+          }
+        }).map((val, key) => {
+          return(
+            <div>
+              <ul>
+                <li>
+                  <a href="http://127.0.0.1:8000/submit">{val.name}</a>
+                </li>
+              </ul>
+            </div>
+          ) 
+        })}
+      </form>
     </div>
   );
 }
